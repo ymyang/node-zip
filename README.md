a6e3b01ec156a286a00843434836588bf71c902e
ymyang <ymyang@163.com>
2015-12-01 21:13:13 +0800

    version 1.0.1

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # node-zip
-zip and unzip for node
+zip and unzip folder for node
+
+# 安装
+
+```
+npm install node-zip-dir
+```
+
+# 使用
+
+```
+var zip = require('node-zip-dir');
+
+// 压缩
+zip.zip('E:/Temp', 'E:/Temp.zip').then(function() {
+    console.log('success');
+}).catch(function(err) {
+    console.error(err);    
+});
+
+// 解压
+zip.unzip('E:/Temp.zip', 'E:/Temp1').then(function() {
+    console.log('success');
+}).catch(function(err) {
+    console.error(err);    
+});
+
+```
+
+
+
--- test/ZipTest.js
+++ test/ZipTest.js
@@ -4,22 +4,14 @@
 var zip = require('../index.js');
 
 describe('zip', function() {
-    it('zip', function(done) {
-        zip.zip({
-            src: 'E:/Temp/*.jpg',
-            dest: 'E:/Temp.zip'
-        }).then(function() {
+    it.only('zip', function(done) {
+        this.timeout(0);
+        zip.zip('E:/Temp', 'E:/Temp.zip').then(function() {
             done();
         }).catch(done);
     });
-    it.only('unzip', function(done) {
-        zip.unzip({
-            src: 'E:/Temp.zip',
-            dest: 'E:/Temp1',
-            filter: function(f) {
-                return f.type !== "SymbolicLink";
-            }
-        }).then(function() {
+    it('unzip', function(done) {
+        zip.unzip('E:/Temp.zip', 'E:/Temp1').then(function() {
             done();
         }).catch(done);
     });
